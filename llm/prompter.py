2bbc3e26d41c75b9c81b99ca9e26f04aabea5066
maciejgogulski <maciej.gogulski1@gmail.com>
2025-05-21 18:27:48 +0200

    Configured logging.

--- llm/prompter.py
+++ llm/prompter.py
@@ -3,11 +3,14 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 import os
+import logging
 
 MODEL = os.getenv('MODEL', 'ollama2')
 
-def prompt(user_prompt):
+log = logging.getLogger(__name__)
 
+def prompt(user_prompt):
+    log.debug(f"Constructing prompt for user input: [{user_prompt}]")
     # Initialize TinyLlama via Ollama
     llm = Ollama(model=MODEL)
 
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from config.logging_conf import setup_logging
+setup_logging()
+
 import os
+import logging
 from flask import Flask, request, jsonify
 from llm.prompter import prompt
 
@@ -11,9 +15,11 @@ app = Flask(__name__)
 app.config['SERVER_PORT'] = os.getenv('SERVER_PORT', '5000')
 debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
 
+log = logging.getLogger(__name__)
 
 @app.route('/prompt', methods=['POST'])
 def prompt_route():
+    log.info("Recieved prompt request")
     data = request.get_json()
     if not data or 'prompt' not in data:
         return jsonify({"error": "Missing 'prompt' in request body"}), 400
