445b199f46dc24f3bbb92d806e9b0f0d02e47190
maciejgogulski <maciej.gogulski1@gmail.com>
2025-05-23 15:05:49 +0200

    Adding file metadata to db when uploading document. Fetching this metadada from DB.

--- routes/documents.py
+++ routes/documents.py
@@ -1,13 +1,14 @@
 import logging
 import os
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from config.db_conf import execute, query
 
 bp = Blueprint('documents', __name__, url_prefix='/documents')
 
 LOG = logging.getLogger(__name__)
 DOCUMENT_STORAGE_PATH = os.getenv('DOCUMENT_STORAGE_PATH', './uploads')
 
-@bp.route('/upload-document', methods=['POST'])
+@bp.route('/', methods=['POST'])
 def upload_document():
     LOG.info("Recieved upload-document request")
     uploaded_file = request.files.get('file')
@@ -16,5 +17,39 @@ def upload_document():
         LOG.info(f"Saving file: {uploaded_file.filename}")
         save_path = f"{DOCUMENT_STORAGE_PATH}/{uploaded_file.filename}"
         uploaded_file.save(save_path)
+
+        try:
+            execute("""
+                  INSERT INTO documents
+                  (filename, filepath)
+                  VALUES
+                  (%(filename)s, %(filepath)s)
+                """, 
+                {
+                      "filename": uploaded_file.filename,
+                      "filepath": save_path
+                }
+            )
+        except Exception as ex: 
+            LOG.error(f"DB error occured: {ex}")
+            
+            return "DB error", 500
+        
         return f"Saved to {save_path}", 200
     return "Invalid file", 400
+
+@bp.route('/', methods=['GET'])
+def get_documents():
+    try:
+        documents = query("""
+            SELECT filename, 
+                filepath, 
+                DATE_FORMAT(added_at, '%Y-%m-%d %H:%i:%s') AS added_at,
+                DATE_FORMAT(updated_at, '%Y-%m-%d %H:%i:%s') AS updated_at
+            FROM documents
+        """)
+    except Exception as ex:
+        LOG.error(f"DB error occured: {ex}")
+        return "DB error", 500
+
+    return jsonify(documents)
